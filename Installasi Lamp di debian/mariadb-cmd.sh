00a228e63780a978ff50286a2c0bd32c598f9e93
Wildy Sheverando <135487348+wildy658@users.noreply.github.com>
2023-08-28 23:43:57 +0700

    Update mariadb-cmd.sh

--- Installasi Lamp di debian/mariadb-cmd.sh
+++ Installasi Lamp di debian/mariadb-cmd.sh
@@ -1,6 +1,6 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON . TO 'root'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 CREATE USER 'phpmyadmin'@'localhost' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON . TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'phpmyadmin'@'localhost' WITH GRANT OPTION;
 SOURCE /usr/share/phpmyadmin/sql/create_tables.sql;
 FLUSH PRIVILEGES;
