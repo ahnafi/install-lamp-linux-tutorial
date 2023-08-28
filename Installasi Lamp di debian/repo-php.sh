bf1bca63fccc40b59af2f607c0ae75c472276af4
Wildy Sheverando <135487348+wildy658@users.noreply.github.com>
2023-08-28 23:17:29 +0700

    Update repo-php.sh

--- Installasi Lamp di debian/repo-php.sh
+++ Installasi Lamp di debian/repo-php.sh
@@ -1,5 +1,5 @@
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc | tail -n1) main" | tee /etc/apt/sources.list.d/php.list
 
 apt install php8.2 php8.2-mysql php8.2-cli php8.2-opcache php8.2-gd \
 php8.2-curl php8.2-cli php8.2-imap php8.2-mbstring php8.2-intl php8.2-soap \
